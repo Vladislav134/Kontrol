86c8b7c5ce9bc51d18402b3922b9e408b63881c8
Vladislav134 <137281737+Vladislav134@users.noreply.github.com>
2024-10-06 12:27:16 +0300

    almost

--- HW1/Program.cs
+++ HW1/Program.cs
@@ -1,3 +1,40 @@
 ﻿using System.Data;
+using System.Numerics;
+string[] Matrix(int n)
+{
+    string[] strArr = new string[n];
+    for (int i = 0; i < n; i++)
+    {
+    strArr[i] = Console.ReadLine()!;
+    }
+    return strArr;
+}
 
-string[] words = new string [] {"one", "two", "three", "four"};
+void PrintMatrix (string[] strArr)
+{
+    for (int i = 0; i < strArr.Length; i++)
+    Console.Write($"\"{strArr[i]}\"  ");
+}
+
+
+string[] Filtr (int n, string[] strArr)
+{
+ string[] Arr = new string[n];
+ for (int i = 0; i < n; i++) 
+ {
+    if (strArr[i].Length < 4)
+    
+    Arr[i] = strArr[i];
+    
+    Console.Write($"\"{Arr[i]}\" ");
+ }
+  return Arr;
+ }
+
+
+Console.WriteLine("Введите количество строк");
+int n = int.Parse(Console.ReadLine()!);
+string[] kool = Matrix(n);
+PrintMatrix(kool);
+Console.Write("->  ");
+Console.WriteLine(Filtr(n, kool));
